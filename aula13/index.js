d91d3a69e952ed4d0f2accb94f8f6c2fa5fe29f1
luanmenezessilva <luanmenezessilva@outlook.com>
2021-11-15 15:45:13 -0300

    Aula 13

---
+++ aula13/index.js
@@ -0,0 +1,14 @@
+const mongodb = require('mongodb').MongoClient
+const url = "mongodb+srv://lmsilva:lmsilva@cluster0.qfkdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongodb.connect(url, (erro, banco)=>{
+    if (erro) throw erro;
+    const dbo = banco.db('cursos')
+    const obj = {curso: 'Curso de Python', canal: 'Pessoal'}
+    const colecao = 'cursos'    
+    dbo.collection(colecao).insertOne(obj, (erro, resultado)=>{
+        if(erro) throw erro
+        console.log("1 novo curso inserido")
+        banco.close()
+    })
+})
